e3af509f6876ca911ddd20dcba539360aee090e2
robert-snigaroff <robertsnigaroff@gmail.com>
2024-01-08 19:46:06 -0500

    added css styling. fixed keyerror on index page

--- forum/__init__.py
+++ forum/__init__.py
@@ -33,7 +33,7 @@ def create_app(test_config=None):
     # Register blueprints
     app.register_blueprint(auth.bp)
     # app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    # app.add_url_rule('/', endpoint='index')
     
     
     return app
